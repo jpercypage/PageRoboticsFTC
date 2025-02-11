9eabae7921f420abd80b5e9f158b8e35744da552
oliverbeckon <oliverbeckon@gmail.com>
2025-02-11 12:10:20 -0700

    Complete refactor of teleop components so that they are basically just a wrapper for the auto versions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/driveModes/DriveMotorsTELE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/driveModes/DriveMotorsTELE.java
@@ -1,60 +1,81 @@
 package org.firstinspires.ftc.teamcode.components.driveModes;
 
+import static org.firstinspires.ftc.teamcode.components.driveModes.DriveMotorsAUTO.ticksPerDegree;
+import static org.firstinspires.ftc.teamcode.components.driveModes.DriveMotorsAUTO.ticksPerInch;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import java.util.HashMap;
-import java.util.Objects;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.components.Camera;
+import org.firstinspires.ftc.teamcode.components.lifts.LiftAUTO;
+
 
 public class DriveMotorsTELE {
 
+    private int LEFTFRONT = 0;
+    private int RIGHTFRONT = 1;
+    private int RIGHTREAR = 2;
+    private int LEFTREAR = 3;
+
+    private double SPEED = 0.5;
+    public boolean isBusy = false;
+    DcMotor[] motors;
+    private final DriveMotorsAUTO driveModeAuto;
+    private final LiftAUTO liftAUTO;
 
 
-        private final HashMap<String, DcMotor> motors = new HashMap<>();
+    public DriveMotorsTELE(HardwareMap map, Telemetry tele) {
+
+        this.driveModeAuto = new DriveMotorsAUTO(map, tele);
+        this.liftAUTO = new LiftAUTO(map, tele);
+        this.motors = this.driveModeAuto.motors;
+
+    }
 
 
-        public DriveMotorsTELE(HardwareMap map) {
-            try {
-                // take in hardware map as param and then use that instead of our own
-                this.motors.put("leftFront", map.get(DcMotor.class, "frontLeft"));
-                this.motors.put("rightFront", map.get(DcMotor.class, "frontRight"));
-                this.motors.put("leftRear", map.get(DcMotor.class, "rearLeft"));
-                this.motors.put("rightRear", map.get(DcMotor.class, "rearRight"));
-            } catch (Exception e) {
-                //throw new RuntimeException(new Throwable("Failed to init DriveMotors. Check connections or configuration naming"));
-                throw new RuntimeException(e);
-            }
-
-            getMotor("rightFront").setDirection(DcMotorSimple.Direction.REVERSE);
-            getMotor("rightRear").setDirection(DcMotorSimple.Direction.REVERSE);
-        }
-
-
-
-        /**
-         * drives with a custom speed
-         *
-         * @param forward -1.00 = max backwards. : 1.00 = max forwards.
-         * @param strafe  -1.00 = max left. : 1.00 = max right.
-         * @param turn    -1.00 = rotate max left. : 1.00 = rotate max right.
-         */
-        public void controls(double forward, double strafe, double turn, boolean isLiftUp) {
-
-            double speed = 1.0D;
-            if (isLiftUp) {
-                speed = 0.25D;
-            }
-            getMotor("leftFront").setPower((forward + strafe + turn) * speed);
-            getMotor("rightFront").setPower((forward - strafe - turn) * speed);
-            getMotor("leftRear").setPower((forward - strafe + turn) * speed);
-            getMotor("rightRear").setPower((forward + strafe - turn) * speed);
-        }
+    /**
+     * drives with a custom speed
+     *
+     * @param forward -1.00 = backwards. : 1.00 = forwards.
+     * @param strafe  -1.00 = left. : 1.00 = right.
+     * @param turn    -1.00 = rotate left. : 1.00 = rotate right.
+     */
+    public void controls(double forward, double strafe, double turn) {
+        this.motors[LEFTFRONT]  .setPower((forward + strafe + turn) * SPEED);
+        this.motors[RIGHTFRONT] .setPower((forward - strafe - turn) * SPEED);
+        this.motors[RIGHTREAR]  .setPower((forward + strafe - turn) * SPEED);
+        this.motors[LEFTREAR]   .setPower((forward - strafe + turn) * SPEED);
+    }
 
 
 
-        private DcMotor getMotor(String motor) {
-            return Objects.requireNonNull(this.motors.get(motor));
-        }
+    public void runAuto() {
+        // Set isBusy to true so that no other inputs are handled until auto is done
+        isBusy = true;
+
+        // Drive to april tag and position under basket
+        driveModeAuto.driveToTag();
+        driveModeAuto.strafe(4);
+        driveModeAuto.rotate(45);
+        driveModeAuto.run(DriveMotorsAUTO.RunModes.EXACT);
+
+        // Dump into top basket
+        liftAUTO.raise();
+        liftAUTO.dump();
+        liftAUTO.lower();
+        liftAUTO.run();
+
+        // Get out of corner
+        driveModeAuto.drive(-8);
+        driveModeAuto.rotate(180);
+        driveModeAuto.run(DriveMotorsAUTO.RunModes.SHORTESTPATH);
+
+        isBusy = false;
+    }
+
+
+
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/lifts/LiftTELE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/lifts/LiftTELE.java
@@ -6,81 +6,35 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import utils.timer;
+
 public class LiftTELE {
 
-    private final Telemetry telemetry;
-    private final DcMotor liftMotor;
-    private final Servo bucket;
+    LiftAUTO lift;
     public LiftTELE(HardwareMap map, Telemetry telemetry) {
-        try {
-            this.telemetry = telemetry;
-            this.liftMotor = map.get(DcMotor.class, "liftMotor");
-            this.bucket = map.get(Servo.class, "bucket");
-        } catch (Exception e) {
-            throw new RuntimeException(new Throwable("lift or bucket motor couldn't initialize. Check connections or configuration naming"));
-        }
-
-        this.liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        this.liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        this.telemetry.addData("Lift: ", this.liftMotor.getCurrentPosition());
-        this.telemetry.update();
+        this.lift = new LiftAUTO(map, telemetry);
     }
     /**
      * Raises the lift to the top
      */
     public void raise() {
 
-        this.liftMotor.setTargetPosition(-3900);
-        this.liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        this.liftMotor.setPower(1D);
+        lift.RUNraise();
     }
-
-
 
     /**
      * lowers the lift to the bottom
      */
     public void lower() {
 
-        this.liftMotor.setTargetPosition(0);
-        this.liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        this.liftMotor.setPower(-0.3D);
-    }
-
-    /**
-     * lowers the lift to the bottom
-     * @param speed (Optional) determines the speed for lowering the lift. 0.0 - 1.0  default is 0.3
-     */
-    public void lower(double speed) {
-        this.telemetry.addData("Lift: ", this.liftMotor.getCurrentPosition());
-
-        this.liftMotor.setTargetPosition(0);
-        this.liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        this.liftMotor.setPower(-speed);
-
-        this.telemetry.update();
+       lift.RUNlower();
     }
 
     /**
      * Dumps the bucket
      */
     public void dump() {
-        this.bucket.setPosition(1.0D);
+        lift.RUNdump();
+        timer.waitish(1.5);
     }
-
-
-    /**
-     * Resets the bucket after dump
-     */
-    public void resetBucket() {
-        this.bucket.setPosition(0.5D);
-    }
-
-    public boolean isLiftUp() {
-        return (this.liftMotor.getCurrentPosition() < -3700);
-    }
-
-
-
-
 }
